607bdb58148b70e025ff9757f8ea7d161edf7e56
Alumno <Alumno@PC66>
2026-05-05 21:36:20 -0300

    Agregar archivos de proyecto.

---
+++ Practica5Mayo/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Practica5Mayo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.lblT = new System.Windows.Forms.Label();
+            this.lblTotalVentas = new System.Windows.Forms.Label();
+            this.dgvResultados = new System.Windows.Forms.DataGridView();
+            this.IdClienteColumna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ImporteColumna = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblClienteMayor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(159, 276);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(93, 31);
+            this.btnCargar.TabIndex = 0;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click_1);
+            // 
+            // lblT
+            // 
+            this.lblT.AutoSize = true;
+            this.lblT.Location = new System.Drawing.Point(23, 370);
+            this.lblT.Name = "lblT";
+            this.lblT.Size = new System.Drawing.Size(82, 13);
+            this.lblT.TabIndex = 3;
+            this.lblT.Text = "Total de Ventas";
+            // 
+            // lblTotalVentas
+            // 
+            this.lblTotalVentas.AutoSize = true;
+            this.lblTotalVentas.Location = new System.Drawing.Point(137, 370);
+            this.lblTotalVentas.Name = "lblTotalVentas";
+            this.lblTotalVentas.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalVentas.TabIndex = 4;
+            this.lblTotalVentas.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dgvResultados
+            // 
+            this.dgvResultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultados.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IdClienteColumna,
+            this.ImporteColumna});
+            this.dgvResultados.Location = new System.Drawing.Point(12, 12);
+            this.dgvResultados.Name = "dgvResultados";
+            this.dgvResultados.Size = new System.Drawing.Size(283, 258);
+            this.dgvResultados.TabIndex = 5;
+            this.dgvResultados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvResultados_CellContentClick);
+            // 
+            // IdClienteColumna
+            // 
+            this.IdClienteColumna.HeaderText = "Cliente";
+            this.IdClienteColumna.Name = "IdClienteColumna";
+            // 
+            // ImporteColumna
+            // 
+            this.ImporteColumna.HeaderText = "Importe";
+            this.ImporteColumna.Name = "ImporteColumna";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Cliente con Mayor compra";
+            // 
+            // lblClienteMayor
+            // 
+            this.lblClienteMayor.AutoSize = true;
+            this.lblClienteMayor.Location = new System.Drawing.Point(189, 410);
+            this.lblClienteMayor.Name = "lblClienteMayor";
+            this.lblClienteMayor.Size = new System.Drawing.Size(0, 13);
+            this.lblClienteMayor.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 450);
+            this.Controls.Add(this.lblClienteMayor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvResultados);
+            this.Controls.Add(this.lblTotalVentas);
+            this.Controls.Add(this.lblT);
+            this.Controls.Add(this.btnCargar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.Label lblT;
+        private System.Windows.Forms.Label lblTotalVentas;
+        private System.Windows.Forms.DataGridView dgvResultados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblClienteMayor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IdClienteColumna;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ImporteColumna;
+    }
+}
+
---
+++ Practica5Mayo/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace Practica5Mayo
+{
+    public partial class Form1 : Form
+    {
+        List<Cliente> clientes = new List<Cliente>();
+        List<Venta> ventas = new List<Venta>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void dgvResultados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        public class Cliente
+        {
+            public int IdCliente { get; set; }
+            public string Nombre { get; set; }
+        }
+
+        public class Venta
+        {
+            public int IdVenta { get; set; }
+            public int IdCliente { get; set; }
+            public decimal Importe { get; set; }
+        }
+
+        private void btnCargar_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                // Limpiar listas
+                clientes.Clear();
+                ventas.Clear();
+
+                // Leer clientes
+                foreach (var linea in File.ReadAllLines("clientes.txt"))
+                {
+                    var datos = linea.Split(',');
+                    clientes.Add(new Cliente
+                    {
+                        IdCliente = int.Parse(datos[0]),
+                        Nombre = datos[1]
+                    });
+                }
+
+                // Leer ventas
+                foreach (var linea in File.ReadAllLines("ventas.txt"))
+                {
+                    var datos = linea.Split(',');
+                    ventas.Add(new Venta
+                    {
+                        IdVenta = int.Parse(datos[0]),
+                        IdCliente = int.Parse(datos[1]),
+                        Importe = decimal.Parse(datos[2])
+                    });
+                }
+
+                // Relacionar datos y mostrar en la grilla
+                var consulta = from v in ventas
+                               join c in clientes on v.IdCliente equals c.IdCliente
+                               select new { Cliente = c.Nombre, v.Importe };
+
+                // Limpiar la grilla antes de cargar
+                dgvResultados.Rows.Clear();
+
+                foreach (var v in ventas)
+                {
+                    var cliente = clientes.FirstOrDefault(c => c.IdCliente == v.IdCliente);
+
+                    if (cliente != null)
+                    {
+                        // Agregar fila con Cliente e Importe
+                        dgvResultados.Rows.Add(cliente.Nombre, v.Importe);
+                    }
+                }
+
+
+                // Total general
+                decimal total = ventas.Sum(v => v.Importe);
+                lblTotalVentas.Text = "Total de Ventas: " + total;
+
+                // Cliente con mayor compra
+                var totalPorCliente = consulta
+                    .GroupBy(x => x.Cliente)
+                    .Select(g => new { Cliente = g.Key, Total = g.Sum(x => x.Importe) });
+
+                var mayorCompra = totalPorCliente.OrderByDescending(x => x.Total).First();
+                lblClienteMayor.Text = $"Cliente con Mayor compra: {mayorCompra.Cliente} ({mayorCompra.Total})";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar datos: " + ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
